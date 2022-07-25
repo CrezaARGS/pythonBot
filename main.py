7ccd662b25506044683521157671835da20c4b07
CrezaARGS <107704959+CrezaARGS@users.noreply.github.com>
2022-07-25 03:05:35 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+from email import message
+import discord 
+from discord.ext import commands
+
+bot = commands.bot(command_prefix="PREFİX ") 
+
+@bot.event
+async def on_ready():
+    print("Ben hazırım")
+
+    @bot.command()
+    async def creza(message)
+    await message.send('https://discord.gg/dhttXfZYXB')
+
+    bot.run("BOT TOKEN")
